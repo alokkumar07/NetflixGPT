c2eef8b92ba4e392fa2091b72a7cfb873a9f5dcb
alok kumar <Alok70.kumar@ril.com>
2023-10-01 13:25:18 +0530

    -Bugfix: If the user is not logged in Redirect /browse to login page and vice versa

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,14 +7,12 @@ import {
   updateProfile,
 } from "firebase/auth";
 import { auth } from "../utils/firebase";
-import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { addUser } from "../utils/userSlice";
 
 const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
   const [errorMessage, setErrorMessage] = useState(null);
-  const navigate = useNavigate();
   const dispatch = useDispatch();
 
   const name = useRef(null);
@@ -61,8 +59,6 @@ const Login = () => {
                   photoURL: photoURL,
                 })
               );
-              navigate("/browse");
-
               // Profile updated!
               // ...
             })
@@ -90,9 +86,6 @@ const Login = () => {
         .then((userCredential) => {
           // Signed in
           const user = userCredential.user;
-
-          console.log(user);
-          navigate("/browse");
 
           // ...
         })
