16e524eafbd2293bf57839b79e9f08c31b963fb5
alok kumar <Alok70.kumar@ril.com>
2023-10-04 16:33:39 +0530

    added css to make responsible

--- src/components/GptSearchBar.js
+++ src/components/GptSearchBar.js
@@ -25,12 +25,13 @@ const GptSearchBar = () => {
 
 
   const handleGptSearchClick = async () => {
-    console.log(searchText.current.value);
+    // console.log(searchText.current.value);
     //makke an api call to OpenAi & get movie results
     const gptQuery =
       "Act as a Movie Recommendation system and suggest some movies for the query" +
       searchText.current.value +
       ".only give me name of 5 movies.,comma separated like the example result given ahead. Example Result: Gadar, Sholay, Don, Golmaal, koi Mil Gya";
+      
     const gptResults = await Openai.chat.completions.create({
       messages: [{ role: "user", content: gptQuery }],
       model: "gpt-3.5-turbo",
@@ -46,9 +47,9 @@ const GptSearchBar = () => {
 
   };
   return (
-    <div className="pt-[10%] flex justify-center">
+    <div className="pt-[35%] md:pt-[10%] flex justify-center">
       <form
-        className="w-1/2 bg-black grid grid-cols-12"
+        className="w-full md:w-1/2 bg-black grid grid-cols-12"
         onSubmit={(e) => e.preventDefault()}
       >
         <input
